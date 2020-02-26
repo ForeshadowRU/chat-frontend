c3eea7550975f24091fdd888993bd741e7facf8b
karina mastyaeva <karina.mastyaeva@gmail.com>
2020-02-26 22:13:52 +0600

    added login form jsx and sass

--- src/modules/Login/index.jsx
+++ src/modules/Login/index.jsx
@@ -1,9 +1,26 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./login.sass";
+
+
 class LoginModule extends Component {
-  render() {
-    return <div className="login-container"> LoginModule Component</div>;
-  }
+    render() {
+        return (
+            <form className="login-container" name="login-container" method="POST">
+                <h1 className="login__title">Welcome back!</h1>
+                <h2 className="login__subtitle">We are glad to see you again</h2>
+                <label className="login__item">
+                    E-mail
+                </label>
+                <input className="login__input" type="email" tabIndex="1"/>
+                <label className="login__item">
+                    Password
+                </label>
+                <input className="login__input" type="password" tabIndex="2"/>
+                <input className="login__button" type="submit" tabIndex="3" value="Log in"/>
+            </form>
+        );
+    }
 }
 
 export default LoginModule;
+
