2e14ac7b92b11bc4f950f29ad1c0d9e175d3bb76
foreshadow <MrJesper@yandex.ru>
2020-04-18 01:33:32 +0600

    [REW] basic styles for layout -- global variables added

--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ServerSidebar from "./ServersBar";
 
 export const DefaultLayout = props => {
   const { children } = props;
   return (
     <>
-      <ServerSidebar />
       <div className="layout-container">{children}</div>
     </>
   );
