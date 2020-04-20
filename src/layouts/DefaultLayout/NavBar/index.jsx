18183f106820e31d9e4c233e6c649ccb593c0023
foreshadow <MrJesper@yandex.ru>
2020-04-20 22:18:38 +0600

    navbar closes on escape pressing

--- src/layouts/DefaultLayout/NavBar/index.jsx
+++ src/layouts/DefaultLayout/NavBar/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import NavItem from "./NavItem";
-import { ReactComponent as CogIcon } from "assets/cog.svg";
-import { ReactComponent as CaretIcon } from "assets/caret.svg";
 import DropdownMenu from "../DropdownMenu";
 import DropdownItem from "../DropdownMenu/DropdownItem";
 import { SvgFactory } from "assets/SvgFactory";
@@ -11,7 +9,7 @@ function NavBar() {
     <nav className="navbar">
       <ul className="navbar-nav">
         <NavItem icon={SvgFactory.bell()}></NavItem>
-        <NavItem icon={<CaretIcon />}>
+        <NavItem icon={SvgFactory.caret()}>
           <DropdownMenu>
             <ProfileItem />
             <DropdownItem leftIcon={SvgFactory.messenger()}>
