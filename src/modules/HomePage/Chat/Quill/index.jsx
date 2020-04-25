4a396fb489aad32a0c72d1edaa6fcbe68e2ec4eb
foreshadow <MrJesper@yandex.ru>
2020-04-25 23:59:54 +0600

    quill added

--- src/modules/HomePage/Chat/Quill/index.jsx
+++ src/modules/HomePage/Chat/Quill/index.jsx
@@ -1,7 +1,49 @@
 import React from "react";
-
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 function Quill() {
-  return <div className="homepage-chat-quill"></div>;
+  const [value, setValue] = useState("");
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  return (
+    <div className="homepage-chat-quill">
+      <ReactQuill
+        className="homepage-chat-quill-container"
+        theme="snow"
+        value={value}
+        modules={modules}
+        formats={formats}
+        onChange={setValue}
+      />
+    </div>
+  );
 }
 
 export default Quill;
