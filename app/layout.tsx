9bd461d20635c29a9d317a80fd6b5b3344cc592d
Ilia Krasov <foreshadowru@gmail.com>
2024-02-13 22:46:28 +0600

    [chore] Удалил старый favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,28 @@ import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'ShadowChat',
+  description: 'Basic Chat App Template',
 };
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID as string}>
+          <MantineProvider theme={theme}>{children}</MantineProvider>
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
